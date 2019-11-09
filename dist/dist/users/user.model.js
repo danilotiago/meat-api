d11d23a1b534971d0d7d797f2079560762b3b4db
danilotiago <programacao.desenvolvimento@gmail.com>
2019-11-09 14:35:02 -0300

    simulando get by id

--- dist/dist/users/user.model.js
+++ dist/dist/users/user.model.js
@@ -1,13 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const users = [
-    { name: 'Nome 1', email: 'nome1@email.com' },
-    { name: 'Nome 2', email: 'nome2@email.com' },
-    { name: 'Nome 3', email: 'nome3@email.com' }
+    { id: '1', name: 'Nome 1', email: 'nome1@email.com' },
+    { id: '2', name: 'Nome 2', email: 'nome2@email.com' },
+    { id: '3', name: 'Nome 3', email: 'nome3@email.com' }
 ];
 class User {
     static all() {
         return Promise.resolve(users);
+    }
+    static findById(id) {
+        return new Promise(resolve => {
+            const user = users.find(user => user.id === id);
+            resolve(user);
+        });
     }
 }
 exports.User = User;
--- dist/users/user.model.ts
+++ dist/users/user.model.ts
@@ -1,12 +1,19 @@
 const users = [
-    {name: 'Nome 1', email: 'nome1@email.com'},
-    {name: 'Nome 2', email: 'nome2@email.com'},
-    {name: 'Nome 3', email: 'nome3@email.com'}
+    {id: '1', name: 'Nome 1', email: 'nome1@email.com'},
+    {id: '2', name: 'Nome 2', email: 'nome2@email.com'},
+    {id: '3', name: 'Nome 3', email: 'nome3@email.com'}
 ];
 
 export class User {
     static all(): Promise<any> {
         return Promise.resolve(users);
+    }
+
+    static findById(id: string): Promise<any> {
+        return new Promise(resolve => {
+            const user = users.find(user => user.id === id);
+            resolve(user);
+        });
     }
 }
 
